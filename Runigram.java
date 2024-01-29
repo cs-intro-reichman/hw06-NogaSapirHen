393e54210e34c4239ac4b3d3a203034bf839339a
NogaSapirHen <noga.sapirhen@post.runi.ac.il>
2024-01-29 18:00:37 +0200

    try

--- Runigram.java
+++ Runigram.java
@@ -174,11 +174,8 @@ public class Runigram {
 		{
 			for ( int j = 0; j < width; j ++)
 			{
-				int y = (int) (i * ((double) h0 / height));
-				int x = (int) (j * ((double) w0 / width));
 				// Gets color from the original image
-				Color originalColor = image[x][y];
-				ScaledImage [i][j] = originalColor;
+				ScaledImage [i][j] = image [(int) (i* ((double) h0 / height))][(int) (j* (double) w0 / width)];
 				}
 			}
 		return ScaledImage;
@@ -194,15 +191,12 @@ public class Runigram {
 		int r = (int) ( alpha * c1.getRed() + (1 - alpha) * c2. getRed());
 		int g = (int) (alpha * c1.getGreen() + (1 - alpha) * c2. getGreen());
 		int b = (int) (alpha * c1.getBlue() + (1 - alpha) * c2. getBlue());
-	//	r = Math.min(Math.max(r, 0), 255);
 		if (r > 255 )
 		r = r-255;
 		if (g > 255 )
 		g = g-255;
 		if (b > 255 )
 		b = b-255;
-	//	g = Math.min(Math.max(g, 0), 255);
-	//	b = Math.min(Math.max(b, 0), 255);
 		Color blended = new Color (r, g, b);
 		return blended;
 	}
@@ -214,6 +208,8 @@ public class Runigram {
 	 * The two images must have the same dimensions.
 	 */
 	public static Color[][] blend(Color[][] image1, Color[][] image2, double alpha) {
+		if (image1.length != image2.length || image1[0].length != image2[0].length)
+		return null;
 		Color[][] BlendImage = new Color[image1.length][image1[0].length];
 		for (int i = 0; i < BlendImage.length; i ++)
 		{
